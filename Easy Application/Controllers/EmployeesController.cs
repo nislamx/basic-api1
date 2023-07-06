4e52fed145d26ba0a9809968a57261aebc165f72
Nobeen Islam <nobeen.islam@xdesign.com>
2023-07-06 11:06:00 +0100

    Add controller

---
+++ Easy Application/Controllers/EmployeesController.cs
@@ -0,0 +1,53 @@
+using Easy_Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Easy_Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetEmployeeById(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(_employeeService.GetById(id));
+        }
+        
+        [HttpPut("Charles/{id}")]
+        public ActionResult<Employee> UpdateCharles(int id)
+        {
+            Employee myEmployee = _employeeService.GetById(id);
+            
+            if (myEmployee.Position == "Senior Charles")
+            {
+                _employeeService.UpdateName(id, "Charles Macgoo");
+                return Ok(myEmployee);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            Employee myEmployee = _employeeService.GetById(id);
+            if (myEmployee.FirstName == "Bertrand")
+            {
+                _employeeService.DeleteById(id);
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
