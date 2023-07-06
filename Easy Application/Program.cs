2289a985a0bc94b20ec39e1452bb4c207338f5a7
Nobeen Islam <nobeen.islam@xdesign.com>
2023-07-06 14:15:43 +0100

    Replace startup with programme

--- Easy Application/Program.cs
+++ Easy Application/Program.cs
@@ -1,9 +1,20 @@
+using Easy_Application.Database;
+using Easy_Application.Profiles;
+using Easy_Application.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("EasyApplicationDb")); // Change this to your actual database connection
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddAutoMapper(typeof(EmployeeProfile));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
